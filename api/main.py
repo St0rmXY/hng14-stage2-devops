61a19dc7df1f6a57b3f3435e5754aa8709ccb163
devops4sure <devops4sure@gmail.com>
2026-04-21 14:35:12 +0100

    fix: Health check added so doesn't always return ok even if Redis is down

--- api/main.py
+++ api/main.py
@@ -3,15 +3,16 @@ import FastAPI
 import redis
 import uuid
 import os
+import HTTPException
 
 app = FastAPI()
-REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_HOST = os.getenv("REDIS_HOST", "rediis")
 r = redis.Redis(host=REDIS_HOST, port=6379)
 
 @app.post("/jobs")
 def create_job():
     job_id = str(uuid.uuid4())
-    r.lpush("job", job_id)
+    r.lpush("job", job_id)i
     r.hset(f"job:{job_id}", "status", "queued")
     return {"job_id": job_id}
 
@@ -19,5 +20,7 @@ def create_job():
 def get_job(job_id: str):
     status = r.hget(f"job:{job_id}", "status")
     if not status:
-        return {"error": "not found"}
+        raise HTTPException{status_code=404, detail=" Job not found"}
     return {"job_id": job_id, "status": status.decode()}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
